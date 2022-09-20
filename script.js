0aff560d913acdf5e8aeeb06a1a2c64ac9b3cec1
Richard Martin <richie@Richards-MacBook-Air.local>
2022-09-19 22:45:26 -0500

    i want to give up

--- script.js
+++ script.js
@@ -7,68 +7,49 @@ var charlength = 8;
 
 // Write password to the #password input
 function passoptions() {
-  let upper = confirm("Do you want uppercase?")
-  console.log(upper)
+  let upper = confirm("Do you want uppercase?");
+  console.log(upper);
 
   let lower = confirm("Do you want lowercase?");
   console.log(lower);
 
   let special = confirm("Do you want special characters?");
-  console.log(special)
+  console.log(special);
 
   let charlength = prompt("How long would you like your password (8-128)");
   console.log(charlength)
 
-
-  var letterbox = {
-    upper: upper,
-    lowercase: lowercase,
-    specialchar: specialchar,
-    charlength: charlength
+  var letterbox = []
+  if (upper) {
+    letterbox = letterbox.concat(uppercase);
+  };
+  if (lower) {
+    letterbox = letterbox.concat(lowercase);
+  };
+  if (special) {
+    letterbox = letterbox.concat(specialchar)
   }
+  console.log(letterbox);
 };
-
-function random(array) {
-  var randomi
-  // Math.floor(), 
-  randomi = Math.random() * array.length
-  var icount = array[randomi]
-  return icount;
-};
-
-function generatePassword() {
-  var square = passoptions()
-  let passresult = []
-  let option = []
-  let answers = []
-  if (true) {
-    letterbox.options
-  }
-if (true) {
-  letterbox.answers
-};
-
-if (true) {
   
-};
-function writePassword() {
-  passoptions();
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  function writePassword() {
+    passoptions();
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+    var password = Math.floor(Math.random() * letterbox.length); 
+    var passwordText = letterbox[i]
+    // if (isNaN(charlength) || charlength < 0 || charlength > 128) {
+    //   alert("Has to be 8 through 128")
 
-  if (isNaN(charlength) || charlength < 0 || charlength > 128) {
-    alert("Has to be 8 through 128")
-
-    return false;
-
+    //   return true;
+    // };
 
   };
 
-};
+  passoptions();
+  passoptions();
 
-passoptions();
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword,);
-console.log("button clicked")};
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  console.log("button clicked")
